d72ccf0ebce5bb3e7e7875c9d6480774085815e5
Johan Compagner <jcompagner@servoy.com>
2022-08-08 14:39:17 +0200

    Better error reporting, override the 3 printStackTrace methods to unwrap the wrapped value and if that is a throwable make sure also the print that.

--- src/org/mozilla/javascript/JavaScriptException.java
+++ src/org/mozilla/javascript/JavaScriptException.java
@@ -8,6 +8,9 @@
 
 package org.mozilla.javascript;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
 /**
  * Java reflection of JavaScript exceptions. Instances of this class are thrown by the JavaScript
  * 'throw' keyword.
@@ -95,6 +98,39 @@ public class JavaScriptException extends RhinoException {
     public int getLineNumber() {
         return lineNumber();
     }
+
+    public void printStackTrace() {
+		super.printStackTrace();
+		Object v = value;
+		if (v instanceof Wrapper) {
+			v = ((Wrapper) v).unwrap();
+		}
+		if (v instanceof Throwable) {
+			((Throwable) v).printStackTrace();
+		}
+	}
+
+	public void printStackTrace(PrintStream s) {
+		super.printStackTrace(s);
+		Object v = value;
+		if (v instanceof Wrapper) {
+			v = ((Wrapper) v).unwrap();
+		}
+		if (v instanceof Throwable) {
+			((Throwable) v).printStackTrace(s);
+		}
+	}
+
+	public void printStackTrace(PrintWriter s) {
+		super.printStackTrace(s);
+		Object v = value;
+		if (v instanceof Wrapper) {
+			v = ((Wrapper) v).unwrap();
+		}
+		if (v instanceof Throwable) {
+			((Throwable) v).printStackTrace(s);
+		}
+	}
 
     private Object value;
     private String details;
