fcfe50b511f48d7a04428be8d54c87c46e82af13
Johan Compagner <jcompagner@servoy.com>
2025-06-02 15:29:03 +0200

    SVY-20285 QB Select throws error in 2025.06
    a quick test for Wrapper objects that already an instanceof that type
    like NativeWrapper which is a Wrapper and a List
    so a list should just be directly returned, but don't do this for Object
    parameter,  because everything is a type of that. (NativeJavaObject is
    also an Object)

--- rhino/src/main/java/org/mozilla/javascript/NativeJavaObject.java
+++ rhino/src/main/java/org/mozilla/javascript/NativeJavaObject.java
@@ -518,8 +518,12 @@ public class NativeJavaObject implements Scriptable, SymbolScriptable, Wrapper, 
 
     /** Type-munging for field setting and method invocation. Conforms to LC3 specification */
     static Object coerceTypeImpl(Class<?> type, Object value) {
-        if (value != null && value.getClass() == type ||
-        		type.isInstance(value)) {
+        if ( (value != null && value.getClass() == type) ||
+        		 // a quick test for Wrapper objects that already an instanceof that type 
+        		// like NativeWrapper which is a Wrapper and a List
+        		// so a list should just be directly returned, but don't do this for Object parameter, 
+        		// because everything is a type of that. (NativeJavaObject is also an Object) 
+        		(!(type instanceof Object) && type.isInstance(value))) {
             return value;
         }
 
